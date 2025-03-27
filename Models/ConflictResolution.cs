0ea866750718a079789ef6dfb591fd54ef05b355
Bogdan-Miron Madose <bogdan.madose@continental-corporation.com>
2025-03-27 12:47:58 +0200

    Add conflict resolution and file transfer options classes
    - Introduced `ConflictResolution` class for managing file operation conflicts.
    - Added `FileTransferOptions` class with properties for selection and file paths.

---
+++ Models/ConflictResolution.cs
@@ -0,0 +1,9 @@
+﻿namespace CopyPath___Modular_MAUI_.Models
+{
+    public class ConflictResolution
+    {
+        public bool ShouldProceed { get; set; }  // true=overwrite, false=skip
+        public bool ApplyToAll { get; set; }     // whether to apply to all files
+        public bool Canceled { get; set; }       // whether operation was canceled
+    }
+}
---
+++ Models/FileTransferOptions.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CopyPath___Modular_MAUI_.Models
+{
+    public partial class FileTransferOptions : ObservableObject
+    {
+        private bool _isSelected;
+
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set => SetProperty(ref _isSelected, value);
+        }
+
+        public required string Name { get; set; }
+        public required string Source { get; set; }
+        public required string Destination { get; set; }
+    }
+}
